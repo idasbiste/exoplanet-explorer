21dcc304501fce87eff07206d31b9940c9e3a1a7
Eduardo Fernandes <idasbistefernandes@gmail.com>
2016-04-27 22:20:22 +0000

    Fetch and show planet

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,11 +59,18 @@ Instructions:
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Uncomment the next line and start here when you're ready to add the first thumbnail!
-
-    Your code goes here!
-     */
-    // getJSON('../data/earth-like-results.json')
+    getJSON('data/earth-like-results.json')
+    .then(function (response) {
+      addSearchHeader(response.query);
+      return getJSON(response.results[0]);
+    })
+    .catch(function () {
+      throw Error("Search request error");
+    })
+    .then(createPlanetThumb)
+    .catch(function (error) {
+      console.log(error);
+      addSearchHeader("unknown");
+    });
   });
 })(document);
